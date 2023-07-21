266e144a2ab57cd35d0a459c949e8378ac0e91bc
Harsh Parmar <harsh.p@netxd.com>
2023-07-21 18:50:50 +0530

    Hello I am doing first commit

---
+++ src/main/java/com/bookmarker/bookmarkerapis/BookmarkerApisApplication.java
@@ -0,0 +1,13 @@
+package com.bookmarker.bookmarkerapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkerApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkerApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookmarker/bookmarkerapis/BookmarkerApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarker.bookmarkerapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkerApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
